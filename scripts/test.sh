dfc84a32481213542e621bd91e8ef184cabde068
petervaro <hello@petervaro.com>
2024-07-27 00:05:46 +0100

    Force Coloured Output in CI

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,19 +5,22 @@ set -euo pipefail;
 
 function run {
     local version="${1-}";
+    local colour="${2-}";
 
-    bash -c "cargo $version test";
-    cd dummy && bash -c "cargo $version test";
+    local command="cargo $version test --color $colour"
+
+    bash -c "$command";
+    cd dummy && bash -c "$command";
 }
 
 ARGUMENT="${1-}";
 case "$ARGUMENT" in
     'ci')
-        run;
+        run '' always;
         ;;
     'local')
-        run +1.79.0;
-        run +1.80.0;
+        run +1.79.0 auto;
+        run +1.80.0 auto;
         ;;
     *)
         printf \
